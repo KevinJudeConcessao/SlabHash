8c2a5b7843ab951397ba2a231d6250d8a2de9390
Kevin Jude Concessao <kevinjudeconcessao@gmail.com>
2021-12-25 14:52:12 +0530

    Operations for counting slabs and elements in PhaseConcurrentMap.

---
+++ src/phase_concurrent_map/device/count_kernel.cuh
@@ -0,0 +1,91 @@
+/*
+ * Copyright 2018 Saman Ashkiani
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or
+ * implied. See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+#ifndef PCMAP_COUNT_KERNEL_H_
+#define PCMAP_COUNT_KERNEL_H_
+
+template <typename KeyT, typename ValueT>
+__global__ void count_key(
+    KeyT* TheKeys,
+    uint32_t* KeyCounts,
+    uint32_t NumberOfQueries,
+    GpuSlabHashContext<KeyT, ValueT, SlabHashTypeT::PhaseConcurrentMap> SlabHashCtxt) {
+  uint32_t ThreadID = blockDim.x * blockIdx.x + threadIdx.x;
+  uint32_t LaneID = threadIdx.x & 0x1F;
+
+  if ((ThreadID - LaneID) >= NumberOfQueries)
+    return;
+
+  KeyT TheKey{};
+  uint32_t TheCount = 0;
+  uint32_t TheBucket = 0;
+  bool ToCount = false;
+
+  if (ThreadID < NumberOfQueries) {
+    TheKey = TheKeys[ThreadID];
+    TheBucket = SlabHashCtxt.computeBucket(TheKey);
+    ToCount = true;
+  }
+
+  SlabHashCtxt.countKey(ToCount, LaneID, TheKey, TheCount, TheBucket);
+
+  if (ThreadID < NumberOfQueries)
+    KeyCounts[ThreadID] = TheCount;
+}
+
+template <typename KeyT, typename ValueT>
+__global__ void bucket_count_kernel(
+    GpuSlabHashContext<KeyT, ValueT, SlabHashTypeT::PhaseConcurrentMap> SlabHashCtxt,
+    uint32_t* KeysCount,
+    uint32_t* SlabsCount,
+    uint32_t NumberOfBuckets) {
+  /* Assign one warp for each bucket */
+
+  uint32_t GlobalThreadID = blockDim.x * blockIdx.x + threadIdx.x;
+  uint32_t BlockWarpID = GlobalThreadID >> 5;  // Also the BucketID
+  uint32_t LaneID = threadIdx.x & 0x1F;
+
+  using SlabHashTy = PhaseConcurrentMapT<KeyT, ValueT>;
+
+  if (BlockWarpID >= NumberOfBuckets)
+    return;
+
+  uint32_t KeyCount = 0;
+  uint32_t SlabCount = 1;
+  uint32_t CurrentSlabPtr = SlabHashTy::A_INDEX_POINTER;
+
+  uint32_t LaneKey = *getPointerFromBucket(BlockWarpID, LaneID);
+  KeyCount = __popc(__ballot_sync(0xFFFFFFFF, LaneKey != EMPTY_KEY) &
+                    SlabHashTy::REGULAR_NODE_KEY_MASK);
+  CurrentSlabPtr = __shfl_sync(0xFFFFFFFF, LaneKey, SlabHashTy::NEXT_PTR_LANE, 32);
+
+  while (CurrentSlabPtr != SlabHashTy::EMPTY_INDEX_POINTER) {
+    LaneKey = *getPointerFromSlab(CurrentSlabPtr, LaneID);
+
+    KeyCount += __popc(__ballot_sync(0xFFFFFFFF, LaneKey != EMPTY_KEY) &
+                       SlabHashTy::REGULAR_NODE_KEY_MASK);
+    SlabCount += 1;
+
+    CurrentSlabPtr = __shfl_sync(0xFFFFFFFF, LaneKey, SlabHashTy::NEXT_PTR_LANE, 32);
+  }
+
+  if (LaneID == 0) {
+    KeyCounts[BlockWarpID] = KeyCount;
+    SlabsCount[BlockWarpID] = SlabCount;
+  }
+}
+
+#endif  // PCMAP_COUNT_KERNEL_H_
---
+++ src/phase_concurrent_map/warp/count.cuh
@@ -0,0 +1,65 @@
+/*
+ * Copyright 2021 [TODO: Assign Copyright]
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or
+ * implied. See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+#ifndef PCMAP_HASH_CTXT_COUNT_H_
+#define PCMAP_HASH_CTXT_COUNT_H_
+
+template <typename KeyT, typename ValueT>
+__device__ __forceinline__ void
+GpuSlabHashContext<KeyT, ValueT, SlabHashTypeT::PhaseConcurrentMap>::countKey(
+    bool& ToBeSearched,
+    const uint32_t& LaneID,
+    const KeyT& TheKey,
+    uint32_t& TheCount,
+    const uint32_t BucketID) {
+  using SlabHashTypeT = PhaseConcurrentMapT<KeyT, ValueT>;
+
+  uint32_t WorkQueue = 0;
+  uint32_t CurrentSlabPtr = SlabHashT::A_INDEX_POINTER;
+  bool IsHeadSlab = true;
+
+  while ((WorkQueue = __ballot_sync(0xFFFFFFFF, ToBeSearched)) != 0) {
+    CurrentSlabPtr = IsHeadSlab ? A_INDEX_POINTER : CurrentSlabPtr;
+
+    uint32_t SourceLane = __ffs(WorkQueue) - 1;
+    uint32_t SourceBucket = __shfl_sync(0xFFFFFFFF, BucketID, SourceLane, 32);
+    uint32_t RequiredKey =
+        __shfl_sync(0xFFFFFFFF,
+                    *reinterpret_cast<uint32_t *>(reinterpret_cast<uint8_t *>(&TheKey)),
+                    SourceLane,
+                    32);
+    uint32_t Data = (CurrentSlabPtr == A_INDEX_POINTER)
+                        ? *getPointerFromBucket(SourceBucket, LaneID)
+                        : *getPointerFromSlab(CurrentSlabPtr, LaneID);
+
+    uint32_t KeyCountInWarp = __popc(__ballot_sync(0xFFFFFFFF, Data == RequiredKey) &
+                                     SlabHashT::REGULAR_NODE_KEY_MASK);
+
+    if (LaneID == SourceLane)
+      TheCount = TheCount + KeyCountInWarp;
+
+    uint32_t NextPtr = __shfl_sync(0xFFFFFFFF, Data, SlabHashTypeT::NEXT_PTR_LANE, 32);
+    if (NextPtr == SlabHashTypeT::EMPTY_INDEX_POINTER) {
+      if (LaneID == SourceLane)
+        ToBeSearched = false;
+    } else {
+      CurrentSlabPtr = NextPtr;
+      IsHeadSlab = false;
+    }
+  }
+}
+
+#endif  // PCMAP_HASH_CTXT_COUNT_H_
