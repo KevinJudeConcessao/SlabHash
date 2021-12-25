dd4533040afe76f46b3b1f4a1775334024852e4b
Kevin Jude Concessao <kevinjudeconcessao@gmail.com>
2021-12-25 15:42:26 +0530

    Delete kernels for PhaseConcurrentMap

---
+++ src/phase_concurrent_map/device/delete_kernel.cuh
@@ -0,0 +1,44 @@
+/*
+ * Copyright 2021 [TODO: Assign Copyright]
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or
+ * implied. See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+#ifndef PCMAP_DELETE_KERNEL_H_
+#define PCMAP_DELETE_KERNEL_H_
+
+template <typename KeyT, typename ValueT>
+__global__ void delete_table_keys(
+    KeyT* TheKeys,
+    uint32_t NumberOfKeys,
+    GpuSlabHashContext<KeyT, ValueT, SlabHashTypeT::PhaseConcurrentMap> SlabHashCtxt) {
+  uint32_t ThreadID = blockDim.x * blockIdx.x + threadIdx.x;
+  uint32_t LaneID = threadIdx.x & 0x1F;
+
+  if ((ThreadID - LaneID) >= NumberOfQueries)
+    return;
+
+  KeyT TheKey{};
+  uint32_t TheBucket = 0;
+  bool ToDelete = false;
+
+  if (ThreadID < NumberOfKeys) {
+    TheKey = TheKeys[ThreadID];
+    TheBucket = SlabHashCtxt.computeBucket(TheKey);
+    ToDelete = true;
+  }
+
+  SlabHashCtxt.deleteKey(ToDelete, LaneID, TheKey, TheBucket);
+}
+
+#endif // PCMAP_DELETE_KERNEL_H_
---
+++ src/phase_concurrent_map/warp/delete.cuh
@@ -0,0 +1,91 @@
+/*
+ * Copyright 2021 [TODO: Assign Copyright]
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or
+ * implied. See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+#ifndef PCMAP_HASH_CTXT_DELETE_H_
+#define PCMAP_HASH_CTXT_DELETE_H_
+
+template <typename KeyT, typename ValueT>
+__device__ __forceinline__ bool
+GpuSlabHashContext<KeyT, ValueT, SlabHashTypeT::PhaseConcurrentMap>::deleteKey(
+    bool& ToBeDeleted,
+    const uint32_t LaneID,
+    const KeyT& TheKey,
+    const uint32_t BucketID) {
+  using SlabHashT = PhaseConcurrentMapT<KeyT, ValueT>;
+
+  uint32_t WorkQueue = 0;
+  uint32_t CurrentSlabPtr = SlabHashT::A_INDEX_POINTER;
+  bool IsHeadSlab = true;
+  bool DeletionStatus = false;
+
+  while ((WorkQueue = __ballot_sync(0xFFFFFFFF, ToBeDeleted)) != 0) {
+    CurrentSlabPtr = IsHeadSlab ? SlabHashT::A_INDEX_POINTER : CurrentSlabPtr;
+
+    uint32_t SourceLane = __ffs(WorkQueue) - 1;
+    uint32_t SourceBucket = __ballot_sync(0xFFFFFFFF, BucketID, SourceLane, 32);
+    uint32_t* DataPtr = (CurrentSlabPtr == SlabHashT::A_INDEX_POINTER)
+                            ? getPointerFromBucket(SourceBucket, LaneID)
+                            : getPointerFromSlab(CurrentSlabPtr, LaneID);
+    uint32_t Data = *DataPtr;
+    uint32_t ReqKey =
+        __shfl_sync(0xFFFFFFFF,
+                    *reinterpret_cast<uint32_t*>(reinterpret_cast<uint8_t*>(&TheKey)),
+                    SourceLane,
+                    32);
+
+    uint32_t IsFound =
+        __ballot_sync(0xFFFFFFFF, Data == ReqKey) & SlabHashT::REGULAR_NODE_KEY_MASK;
+
+    if (IsFound) {
+      int CandidateLane = __ffs(IsFound) - 1;
+      uint32_t ValuesSlabVAddr =
+          __shfl_sync(0xFFFFFFFF, Data, SlabHashT::VALUES_PTR_LANE, 32);
+      uint32_t MutexSlabVAddr =
+          __shfl_sync(0xFFFFFFFF, Data, SlabHashT::MUTEX_PTR_LANE, 32);
+
+      if (LaneID == SourceLane) {
+        uint32_t* ValuePtr = getPointerFromSlab(ValueSlabVAddr, CandidateLane);
+        uint32_t* MutexPtr = getPointerFromSlab(MutexSlabVAddr, CandidateLane);
+
+        if (atomicCAS(MutexPtr, EMPTY_KEY, 0) == EMPTY_KEY) {
+          uint32_t Key = *Data;
+          if (Key == *reinterpret_cast<uint32_t*>(reinterpret_cast<uint8_t*>(&ReqKey))) {
+            *Data = EMPTY_KEY;
+            *Value = EMPTY_KEY;
+            DeletionStatus = true;
+            ToBeDeleted = false;
+          }
+        }
+      }
+    } else {
+      uint32_t NextSlabPtr = __shfl_sync(0xFFFFFFFF, Data, SlabHashT::NEXT_PTR_LANE, 32);
+
+      if (NextSlabPtr == SlabHashT::EMPTY_INDEX_POINTER) {
+        if (SourceLane == LaneID) {
+          DeletionStatus = false;
+          ToBeDeleted = false;
+        }
+      } else {
+        CurrentSlabPtr = NextSlabPtr;
+        IsHeadSlab = false;
+      }
+    }
+  }
+
+  return DeletionStatus;
+}
+
+#endif  // PCMAP_HASH_CTXT_DELETE_H_
