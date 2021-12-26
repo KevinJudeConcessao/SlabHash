ff37f7e5a0d55d9008abefa5d4289138d0bae268
Kevin Jude Concessao <kevinjudeconcessao@gmail.com>
2021-12-26 21:46:59 +0530

    Upsert operation for PhaseConcurrentMap

--- src/phase_concurrent_map/warp/update.cuh
+++ src/phase_concurrent_map/warp/update.cuh
@@ -50,9 +50,9 @@ __device__ __forceinline__
                             : getPointerFromSlab(CurrentSlabPtr, LaneID);
     uint32_t Data = *DataPtr;
 
-    int FoundLane = __ffs(__ballot_sync(0xFFFFFFFF, Data == ReqKey) &
-                          SlabHashT::REGULAR_NODE_KEY_MASK) -
-                    1;
+    int FoundLane =
+        (__ballot_sync(0xFFFFFFFF, Data == ReqKey) & SlabHashT::REGULAR_NODE_KEY_MASK) -
+        1;
 
     if (FoundLane < 0) {
       uint32_t NextSlabPtr = __shfl_sync(0xFFFFFFFF, Data, SlabHashT::NEXT_PTR_LANE, 32);
@@ -64,7 +64,7 @@ __device__ __forceinline__
         IsHeadSlab = false;
       }
     } else {
-      uint32_t UpdateLane = __ffs(Found & SlabHashT::REGULAR_NODE_KEY_MASK) - 1;
+      const uint32_t UpdateLane = static_cast<uint32_t>(FoundLane);
       uint32_t ValueSlabVAddr =
           __shfl_sync(0xFFFFFFFF, Data, SlabHashT::VALUES_PTR_LANE, 32);
       uint32_t MutexSlabVAddr =
@@ -101,7 +101,7 @@ template <typename KeyT, typename ValueT>
 __device__ __forceinline__
     typename std::enable_if<FilterCheck<FilterTy>::value && MapCheck<MapTy>::value>::type
     GpuSlabHashContext<KeyT, ValueT, SlabHashTypeT::PhaseConcurrentMap>::upsertPair(
-        bool& ToBeUpdated,
+        bool& ToBeUpserted,
         const uint32_t& LaneID,
         const KeyT& TheKey,
         const ValueT& TheValue,
@@ -113,7 +113,121 @@ __device__ __forceinline__
   uint32_t CurrentSlabPtr = SlabHashT::A_INDEX_POINTER;
   bool IsHeadSlab = true;
 
-  while (WorkQueue = __ballot_sync)
+  while (WorkQueue = __ballot_sync(0xFFFFFFFF, ToBeUpserted) != 0) {
+    CurrentSlabPtr = IsHeadSlab ? SlabHashT::A_INDEX_POINTER : CurrentSlabPtr;
+
+    uint32_t SourceLane = __ffs(WorkQueue) - 1;
+    uint32_t SourceBucket = __shfl_sync(0xFFFFFFFF, BucketID, SourceLane, 32);
+    uint32_t ReqKey =
+        __shfl_sync(0xFFFFFFFF,
+                    *(reinterpret_cast<uint32_t*>(reinterpret_cast<uint8_t*>(&TheKey))),
+                    SourceLane,
+                    32);
+
+    uint32_t* DataPtr = (CurrentSlabPtr == SlabHashT::A_INDEX_POINTER)
+                            ? getPointerFromBucket(SourceBucket, LaneID)
+                            : getPointerFromSlab(CurrentSlabPtr, LaneID);
+    uint32_t Data = *DataPtr;
+
+    int FoundLane = __ffs(__ballot_sync(0xFFFFFFFF, Data == ReqKey) &
+                          SlabHashT::REGULAR_NODE_KEY_MASK) -
+                    1;
+    int EmptyLanes =
+        __ballot_sync(0xFFFFFFFF, Data == EMPTY_KEY) & SlabHashT::REGULAR_NODE_KEY_MASK;
+
+    if (FoundLane < 0) {
+      if (EmptyLanes == 0) {
+        uint32_t NextSlabPtr =
+            __shfl_sync(0xFFFFFFFF, Data, SlabHashT::NEXT_PTR_LANE, 32);
+        if (NextSlabPtr == SlabHashT::EMPTY_INDEX_POINTER) {
+          uint32_t NewKeySlabVAddr = allocateSlab(TheAllocatorContext, LaneID);
+          uint32_t NewValueSlabVAddr = allocateSlab(TheAllocatorContext, LaneID);
+          uint32_t NewMutexSlabVAddr = allocateSlab(TheAllocatorContext, LaneID);
+
+          if (LaneID == SlabHashT::MUTEX_PTR_LANE) {
+            uint32_t* MutexLanePtr =
+                getPointerFromSlab(NewKeySlabVAddr, SlabHashT::MUTEX_PTR_LANE);
+            *MutexPtrLane = NewMutexSlabVAddr;
+          }
+
+          if (LaneID == SlabHashT::VALUES_PTR_LANE) {
+            uint32_t* ValuesLanePtr =
+                getPointerFromSlab(NewKeySlabVAddr, SlabHashT::VALUES_PTR_LANE);
+            *ValuesPtrLane = NewValueSlabVAddr;
+          }
+
+          if (LaneID == SlabHashT::NEXT_PTR_LANE) {
+            uint32_t* NextLanePtr =
+                IsHeadSlab ? getPointerFromBucket(SourceBucket, SlabHashT::NEXT_PTR_LANE)
+                           : getPointerFromSlab(CurrentSlabPtr, SlabHashT::NEXT_PTR_LANE);
+
+            uint32_t OldVAddr =
+                atomicCAS(NextLanePtr, SlabHashT::EMPTY_INDEX_POINTER, NewKeySlabVAddr);
+            if (OldVAddr != SlabHashT::EMPTY_INDEX_POINTER) {
+              freeSlab(NewKeySlabVAddr);
+              freeSlab(NewValueSlabVAddr);
+              freeSlab(NewMutexSlabVAddr);
+            }
+          }
+        } else {
+          CurrentSlabPtr = NextSlabPtr;
+          IsHeadSlab = false;
+        }
+      } else {
+        uint32_t InsertLane = __ffs(EmptyLanes & SlabHashT::REGULAR_NODE_KEY_MASK) - 1;
+        uint32_t ValueSlabVAddr =
+            __shfl_sync(0xFFFFFFFF, Data, SlabHashT::VALUES_PTR_LANE, 32);
+        uint32_t MutexSlabVAddr =
+            __shfl_sync(0xFFFFFFFF, Data, SlabHashT::MUTEX_PTR_LANE, 32);
+
+        if (SourceLane == LaneID) {
+          uint32_t* ValuePtr = getPointerFromSlab(ValueSlabVAddr, InsertLane);
+          uint32_t* MutexPtr = getPointerFromSlab(MutexSlabVAddr, InsertLane);
+
+          if (atomicCAS(MutexPtr, EMPTY_KEY, 0) == EMPTY_KEY) {
+            if ((static_cast<uint64_t>(*ValuePtr) << 32 | *DataPtr) == EMPTY_PAIR_64) {
+              *DataPtr =
+                  *reinterpret_cast<uint32_t*>(reinterpret_cast<uint8_t*>(&TheKey));
+              *ValuePtr =
+                  *reinterpret_cast<uint32_t*>(reinterpret_cast<uint8_t*>(&TheValue));
+              ToBeUpserted = false;
+            }
+
+            atomicExch(MutexPtr, EMPTY_KEY);
+          }
+        }
+      }
+    } else {
+      const uint32_t UpdateLane = static_cast<uint32_t>(FoundLane);
+      uint32_t ValueSlabVAddr =
+          __shfl_sync(0xFFFFFFFF, Data, SlabHashT::VALUES_PTR_LANE, 32);
+      uint32_t MutexSlabVAddr =
+          __shfl_sync(0xFFFFFFFF, Data, SlabHashT::MUTEX_PTR_LANE, 32);
+
+      if (LaneID == SourceLane) {
+        uint32_t* ValuePtr = getPointerFromSlab(ValueSlabVAddr, UpdateLane);
+        uint32_t* MutexPtr = getPointerFromSlab(MutexSlabVAddr, UpdateLane);
+
+        if (atomicCAS(MutexPtr, EMPTY_KEY, 0) == EMPTY_KEY) {
+          uint32_t Key = *DataPtr;
+          uint32_t Value = *ValuePtr;
+
+          if (Key ==
+              *(reinterpret_cast<uint32_t*>(reinterpret_cast<uint8_t*>(&TheKey)))) {
+            FilterTy F{};
+            MapTy M{Value};
+
+            if (F(Value))
+              *ValuePtr =
+                  *(reinterpret_cast<uint32_t*>(reinterpret_cast<uint8_t*>(&TheValue)));
+
+            ToBeUpserted = false;
+          }
+          atomicExch(MutexPtr, EMPTY_KEY);
+        }
+      }
+    }
+  }
 }
 
 #endif  // PCMAP_HASH_CTXT_UPDATE_H_
