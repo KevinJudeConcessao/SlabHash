e4b5c59062b8e3226789aedce02781c8ccc11bfe
Kevin Jude Concessao <kevinjudeconcessao@gmail.com>
2022-01-02 16:45:17 +0530

    Move allocator policies to SlabAlloc

--- src/slab_hash_global.cuh
+++ src/slab_hash_global.cuh
@@ -106,29 +106,6 @@ struct __align__(32) PhaseConcurrentValueSlab {
  */
 enum class SlabHashTypeT { ConcurrentMap, ConcurrentSet, PhaseConcurrentMap };
 
-template <uint32_t LogNumMemoryBlocks, uint32_t NumSuperBlocks, uint32_t NumReplicas = 1u>
-struct LightAllocatorPolicy {
-  static constexpr uint32_t LogNumberOfMemoryBlocks = LogNumMemoryBlocks;
-  static constexpr uint32_t NumberOfSuperBlocks = NumSuperBlocks;
-  static constexpr uint32_t NumberOfReplicas = NumReplicas;
-
-  using DynamicAllocatorT =
-      SlabAllocLight<LogNumMemoryBlocks, NumSuperBlocks, NumReplicas>;
-  using AllocatorContextT =
-      SlabAllocLightContext<LogNumMemoryBlocks, NumSuperBlocks, NumReplicas>;
-};
-
-template <uint32_t LogNumMemoryBlocks, uint32_t NumSuperBlocks, uint32_t NumReplicas = 1u>
-struct FullAllocatorPolicy {
-  static constexpr uint32_t LogNumberOfMemoryBlocks = LogNumMemoryBlocks;
-  static constexpr uint32_t NumberOfSuperBlocks = NumSuperBlocks;
-  static constexpr uint32_t NumberOfReplicas = NumReplicas;
-
-  using DynamicAllocatorT = SlabAlloc<LogNumMemoryBlocks, NumSuperBlocks, NumReplicas>;
-  using AllocatorContextT =
-      SlabAllocLight<LogNumMemoryBlocks, NumSuperBlocks, NumReplicas>;
-};
-
 template <typename KeyT, typename ValueT>
 class ConcurrentMapT {
  public:
